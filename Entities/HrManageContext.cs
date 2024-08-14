20dd2fdfd8b4e1858279e063f03919cd80082188
quoj <133200203+quoj@users.noreply.github.com>
2024-08-14 19:54:07 +0700

    Add files via upload

---
+++ Entities/HrManageContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExamInASP.NET_MVC.Entities;
+
+public partial class HrManageContext : DbContext
+{
+    public HrManageContext()
+    {
+    }
+
+    public HrManageContext(DbContextOptions<HrManageContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost,1433;Database=HR_MANAGE;User Id=sa;Password=Password789;TrustServerCertificate=true");
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
